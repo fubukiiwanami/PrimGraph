471db96a38407176931e1bb1f3e2bc9280d216de
Niek <niekversteege@gmail.com>
2012-10-04 00:25:18 +0200

    Init

---
+++ src/nl/niek/minor/aa/primgraph/PrimMain.java
@@ -0,0 +1,15 @@
+package nl.niek.minor.aa.primgraph;
+
+public class PrimMain
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
